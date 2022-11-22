9046ac96bf1afb619c1b1a4e80cc4581bafb2712
paullsshin <paullsshin@hotmail.com>
2022-11-21 18:02:01 -0800

    added variables for so users can choose what to have in their password

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
 
 // Assignment code here
+// creating list of variables for password options
+function generatePassword () {
+    var numbers = [1,2,3,4,5,6,7,8,9,0];
+    var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var special = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","|",";",":","'",",","<",".",".",">","/","?"];
+    var long = [];
 
+    // after list has been made then I need to set parameters for the password
+
+    
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
