9af408f90213a4a22d4b4ef03fec9a2361e264ca
paullsshin <paullsshin@hotmail.com>
2022-11-21 18:17:14 -0800

    added confirms for each option for the user

--- assets/script.js
+++ assets/script.js
@@ -9,8 +9,21 @@ function generatePassword () {
     var long = [];
 
     // after list has been made then I need to set parameters for the password
+    // this first one will determine ther length of the password
+    characterLength = prompt("How many characters would you like to have in your password? Please choose a number between 8 and 128 characters.");
+    if (characterLength < 8 || characterLength > 128) {
+        prompt("Please choose a valid number between 8 and 128!");
+    } else if (isNaN(characterLength)) {
+         prompt ("Please enter a valid number digit.");
+        //  added another parameter for if the user decides to "type" the number
+    }
+    // adding confirms to each option to gather info on what kind of password the user wants
+    digits = confirm ("Would you like to have numbers in your password?");
+    small = confirm ("Would you like to have lower case letters in your password?");
+    big = confirm ("Would you like to have upper case letters in your password?");
+    characters = confirm ("Would you like to have special characters in your password?");
 
-    
+
 }
 
 // Get references to the #generate element
